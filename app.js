b96ea70ea1dcabc7b18ecdc81799154c41a97a7e
“Jarrale <tekibotz@gmail.com>
2024-06-25 13:38:37 -0400

    Module 7 completed baseline

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport');
 
 // Define routers
 const indexRouter = require('./app_server/routes/index');
@@ -14,6 +17,8 @@ const handlebars = require('hbs');
 
 // Bring in the database
 require('./app_api/models/db');
+
+require('./app_api/config/passport');
 
 const app = express();
 
@@ -30,11 +35,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 // Enable CORS
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
@@ -44,6 +50,15 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/api', apiRouter);
+
+// Catch unauthorizated error and create 401
+app.use((err, req, res, next) => {
+  if(err.name === 'UnauthorizedError') {
+    res
+      .status(401)
+      .json({"message": err.name + ": " + err.message});
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -55,35 +55,36 @@ const tripsFindByCode = async(req, res) => {
 // POST: /trips - Adds a new Trip
 // Regardless of outcome, response must include HTML status code
 // and JSON message to the requesting client
-const tripsAddTrip = async(req, res) => {
-    const newTrip = new Trip({
-        code: req.body.code,
-        name: req.body.name,
-        length: req.body.length,
-        start: req.body.start,
-        resort: req.body.resort,
-        perPerson: req.body.perPerson,
-        image: req.body.image,
-        description: req.body.description
-    });
+const tripsAddTrip = async (req, res) => {
+    try {
+        // Call getUser to fetch user information
+        const user = await getUser(req, res);
 
-    const q = await newTrip.save();
+        // Create a new Trip object with request body data
+        const newTrip = new Trip({
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description,
+            // Assuming you want to associate the trip with the user
+            user: user._id  // Replace with the correct user ID field from getUser response
+        });
 
-        if(!q)
-        { // Database returned no data
-            return res
-                .status(400)
-                .json(err);
-        } else { // Return new trip
-            return res
-                .status(201)
-                .json(q);
-        }
+        // Save the new trip to the database
+        const savedTrip = await newTrip.save();
 
-        // Uncomment the folloing line to show results of operation
-        // on the console
-        // console.log(q);
+        // Respond with the saved trip
+        return res.status(201).json(savedTrip);
+    } catch (err) {
+        // Handle errors
+        return res.status(400).json(err);
+    }
 };
+
 
 
 
@@ -91,38 +92,71 @@ const tripsAddTrip = async(req, res) => {
 // Regardless of outcome, response must include HTML status code
 // and JSON message to the requesting client
 const tripsUpdateTrip = async (req, res) => {
-    // Uncomment for debugging
-    console.log(req.params);
-    console.log(req.body);
-    
-    const q = await Model.findOneAndUpdate(
-      { 'code': req.params.tripCode },
-      {
-        code: req.body.code,
-        name: req.body.name,
-        length: req.body.length,
-        start: req.body.start,
-        resort: req.body.resort,
-        perPerson: req.body.perPerson,
-        image: req.body.image,
-        description: req.body.description
-      }
-    ).exec();
-  
-    if (!q) {
-      // Database returned no data
-      return res.status(400).json(err);
-    } else {
-      // Return resulting updated trip
-      return res.status(201).json(q);
-    }
-  
-    // Uncomment the following line to show results of operation
-    // on the console
-    // console.log(q);
-  };
-  
+    getUser(req, res, (req, res) => {
+        Trip.findOneAndUpdate(
+            { 'code': req.params.tripCode },
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            },
+            { new: true }
+        )
+        .then(trip => {
+            if (!trip) {
+                return res.status(404)
+                          .send({
+                              message: "Trip not found with code " + req.params.tripCode
+                          });
+            }
+            res.send(trip);
+        })
+        .catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404)
+                          .send({
+                              message: "Trip not found with code " + req.params.tripCode
+                          });
+            }
+            return res.status(500) // server error
+                      .json(err);
+        });
+    });
+}
 
+
+getUser(req, res, (userName) => {  // Corrected callback structure
+    const locationId = req.params.locationid;
+    if (locationId) {
+      Loc
+        .findById(locationId)
+        .select('reviews')
+        .exec((err, location) => {
+          if (err) {
+            return res
+              .status(400)
+              .json(err);
+          } else if (!location) {
+            return res
+              .status(404)
+              .json({ "message": "Location not found" });
+          } else {
+            doAddReview(req, res, location, userName);
+          }
+        });
+    } else {
+      res
+        .status(404)
+        .json({ "message": "Location ID is required" });
+    }
+  });
+  
+  
 module.exports = {
     tripsList,
     tripsFindByCode,
